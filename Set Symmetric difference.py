d35c909f3909dfe380773007a427d218a4f8fb35
Honeyhavvi2012 <18so0072@odl.silveroaks.co.in>
2025-06-25 18:36:04 +0530

    Add files via upload

---
+++ Set Symmetric difference.py
@@ -0,0 +1,11 @@
+Set1 = {'blue', 'green'}
+Set2 = {'blue', 'yellow'}
+
+result = Set1.symmetric_difference(Set2)
+print(result)
+
+Set1 = {1, 2, 3, 4, 5}
+Set2 = {1, 5, 6, 7, 8, 9}
+
+result = Set1.symmetric_difference(Set2)
+print(result)
